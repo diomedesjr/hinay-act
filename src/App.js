4c8fae2bd5224a04ee68a0c12190d12dce144a44
deejay <hinay.diomedesjr@gmail.com>
2022-06-01 16:40:29 +0800

    third commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Top from './Components/Navbar';
+import 'bootstrap/dist/css/bootstrap.css'
+import { Nav, Navbar } from 'react-bootstrap'
+import logo from './logo.svg'
 
 function App() {
   return (
     <div className="App">
-      
-      <Top />
+      <Navbar bg = "dark" variant = "dark" sticky="top" expand= "sm" collapseOnSelect>
+        <Navbar.Brand>
+        <img src={logo} className="logo" alt="logo"  />
+          Diomedes
+        </Navbar.Brand>
+        
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+        <Nav className="ms-auto">
+          <Nav.Link href="products">Home</Nav.Link>
+          <Nav.Link href="products">About</Nav.Link>
+          <Nav.Link href="products">Contact</Nav.Link>
+          <Nav.Link href="products">Blog</Nav.Link>
+        </Nav>
+        </Navbar.Collapse>
 
+        
+      </Navbar>
+      <div className="content">
+        This is content
+      </div>
     </div>
   );
 }
